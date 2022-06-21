8cef906e7186878406edd4a77193e93ffa26b3cd
hyunie-de <hyunie@tumblbug.com>
2022-06-21 18:25:00 +0900

    ver 1.0.4 update - revise base package psycopg2 -> redshift_connector

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pilot_works
+
+## Red2Df
+    - 기능 및 목적 : Python에서 Redshift를 연결하는 과정을 추상화하여 DB API 사용이 익숙하지 않은 user들의 편의를 높임.
+    - 결과 : PyPI 배포 ([링크](https://pypi.org/project/red2df))
--- red2df/setup.py
+++ red2df/setup.py
@@ -5,15 +5,15 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 setup(
     name='red2df',
-    version='1.0.3',
+    version='1.0.4',
     packages=['red2df'],
     description='Create a Pandas dataframe with your Redshift data',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Hyunie',
     author_email='kyunghyun7843@gmail.com',
-    url='https://github.com/Gyeong-Hyeon/pilot_works/tree/main/redshift_conn',
+    url='https://github.com/Gyeong-Hyeon/pilot_works/tree/main/red2df',
     license='MIT',
     python_requires='>=3',
-    install_requires=['pandas','psycopg2']
+    install_requires=['pandas','redshift_connector']
 )
